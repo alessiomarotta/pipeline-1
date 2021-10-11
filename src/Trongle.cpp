b87f55485f418e5f2ef3b79a9b36729abda5a094
alessiomarotta <alessio.marotta.98@gmail.com>
2021-10-11 22:49:06 +0200

    Trongle

---
+++ src/Trongle.cpp
@@ -0,0 +1,8 @@
+#include "Trongle.h"
+#include "Vertex.h"
+
+Trongle::Trongle(Vertex a, Vertex b, Vertex c) {
+	this->a = a;
+	this->b = b;
+	this->c = c;
+}
---
+++ src/Trongle.h
@@ -0,0 +1,15 @@
+#ifndef TRONGLE_H
+#define TRONGLE_H
+
+#include "Vertex.h"
+
+class Trongle {
+	public:
+		Vertex a;
+		Vertex b;
+		Vertex c;
+
+		Trongle(Vertex a, Vertex b, Vertex c);
+};
+
+#endif
