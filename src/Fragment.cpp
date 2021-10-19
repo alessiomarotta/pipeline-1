9a9794f8393792efb49075acac6640491252ab77
alessiomarotta <alessio.marotta.98@gmail.com>
2021-10-19 16:35:01 +0200

    Implement Fragment

---
+++ src/Fragment.cpp
@@ -0,0 +1,7 @@
+#include "Fragment.h"
+
+Fragment::Fragment(int x, int y, double z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
---
+++ src/Fragment.h
@@ -0,0 +1,12 @@
+#ifndef FRAGMENT_H
+#define FRAGMENT_H
+
+class Fragment {
+	public:
+		int x, y;
+		double z;
+
+		Fragment(int x, int y, double z);
+};
+
+#endif
